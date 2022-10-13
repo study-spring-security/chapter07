522445f63ff6c598e355b2ddaa2d383ef692271e
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 22:53:43 +0900

    init project

---
+++ src/main/java/com/example/chpater07/Chpater07Application.java
@@ -0,0 +1,13 @@
+package com.example.chpater07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chpater07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chpater07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chpater07/Chpater07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chpater07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chpater07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
