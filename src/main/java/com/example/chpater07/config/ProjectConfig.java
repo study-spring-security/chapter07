6a2e720c564295a63cb127160b33d39de184b58b
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-13 23:05:32 +0900

    feat: add ProjectConfig

---
+++ src/main/java/com/example/chpater07/config/ProjectConfig.java
@@ -0,0 +1,56 @@
+package com.example.chpater07.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+    public static final String READ = "READ";
+    public static final String WRITE = "WRITE";
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manage = new InMemoryUserDetailsManager();
+
+        var user1 = User.withUsername("john")
+                .password("12345")
+                .authorities(READ)
+                .build();
+
+
+        var user2 = User.withUsername("jane")
+                .password("12345")
+                .authorities(WRITE)
+                .build();
+
+        manage.createUser(user1);
+        manage.createUser(user2);
+
+        return manage;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests()
+                .anyRequest()
+                .permitAll();
+
+        return http.build();
+    }
+
+}
